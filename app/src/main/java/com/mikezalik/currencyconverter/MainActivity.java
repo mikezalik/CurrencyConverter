1c33c30310351fd3969879726ffaae1750ee2bcd
mikezalik <mike.zalik@gmail.com>
2020-01-15 13:47:34 -0800

    Adds String and double for amount in pesos, conversion rate as of 01.15.2020 at 1:30pm PST, toast for conversion amount and string format for conversion.

--- app/src/main/java/com/mikezalik/currencyconverter/MainActivity.java
+++ app/src/main/java/com/mikezalik/currencyconverter/MainActivity.java
@@ -5,10 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.lang.String.format;
 
 public class MainActivity extends AppCompatActivity {
 
 public void convertCurrency (View view){
+    EditText editText = findViewById(R.id.editText);
+    String amountInPesos = editText.getText().toString();
+    double amountInPesosDouble = Double.parseDouble(amountInPesos);
+    double amountInDollarsDouble = amountInPesosDouble * 0.053;
+    String convertAmount = format("%.2f", amountInDollarsDouble);
+
+    Toast.makeText(this, "$" + convertAmount, Toast.LENGTH_SHORT).show();
+
     Log.i ("This works!","This also works!");
 
 }
